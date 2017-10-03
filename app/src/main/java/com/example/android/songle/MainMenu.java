e72e68048c655ba86813eb3f1ea970567a3d9087
Matthew McCarrison <s1515679@sms.ed.ac.uk>
2017-10-04 00:14:25 +0100

    Colour changes

--- app/src/main/java/com/example/android/songle/MainMenu.java
+++ app/src/main/java/com/example/android/songle/MainMenu.java
@@ -4,13 +4,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.widget.ProgressBar;
 
 public class MainMenu extends AppCompatActivity {
+
+    private ProgressBar levelPb = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        levelPb = (ProgressBar) findViewById(R.id.level_pb);
+        levelPb.setMax(100);
+        levelPb.setProgress(50);
     }
 
 }
