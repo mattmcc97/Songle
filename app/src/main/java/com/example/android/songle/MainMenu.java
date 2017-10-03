340da7c010485ad4c0abc2e9b09395115221fcf8
Matthew McCarrison <s1515679@sms.ed.ac.uk>
2017-10-03 18:22:46 +0100

    Edited appearance of help button and level progress bar

--- app/src/main/java/com/example/android/songle/MainMenu.java
+++ app/src/main/java/com/example/android/songle/MainMenu.java
@@ -2,6 +2,8 @@ package com.example.android.songle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -10,4 +12,5 @@ public class MainMenu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
     }
+
 }
