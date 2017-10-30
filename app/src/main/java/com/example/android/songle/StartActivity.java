ca6835118f524db520fb4d99f690da62f5bfa4d4
Matthew McCarrison <s1515679@sms.ed.ac.uk>
2017-10-30 18:24:56 +0000

    Added info button to StartActivity

--- app/src/main/java/com/example/android/songle/StartActivity.java
+++ app/src/main/java/com/example/android/songle/StartActivity.java
@@ -1,9 +1,13 @@
 package com.example.android.songle;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -11,6 +15,7 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
     }
 
     public void startGame(View view) {
@@ -18,4 +23,23 @@ public class StartActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MainMenu.class);
         startActivity(intent);
     }
+
+    public void infoButtonClicked(View view) {
+
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.help_dialog);
+        dialog.show();
+
+        Button okBtn = (Button) dialog.findViewById(R.id.help_ok_button);
+
+        okBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+    }
+
+
 }
